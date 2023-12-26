7661d30d2bdd7c94ba5f57df395a4ab56db6a982
Orkhan <aliyevorhan82@gmail.com>
2023-12-27 03:45:16 +0400

    write implementation

--- MongoRepositoryPattern.Domain/Context/IMongoDbContext.cs
+++ MongoRepositoryPattern.Domain/Context/IMongoDbContext.cs
@@ -1,6 +1,9 @@
-﻿namespace MongoRepositoryPattern.Domain.Context
+﻿using MongoDB.Driver;
+
+namespace MongoRepositoryPattern.Domain.Context
 {
     public interface IMongoDbContext
     {
+        IMongoDatabase Database { get; }
     }
 }
--- MongoRepositoryPattern.Domain/Context/MongoDbContext.cs
+++ MongoRepositoryPattern.Domain/Context/MongoDbContext.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MongoDB.Driver;
 
 namespace MongoRepositoryPattern.Domain.Context
 {
-    public class MongoDbContext:IMongoDbContext
+    public class MongoDbContext : IMongoDbContext
     {
+        public IMongoDatabase Database { get; private set; }
+
+        public MongoDbContext()
+        {
+            // empty connection string is for local mongo 
+            var client = new MongoClient();
+            this.Database = client.GetDatabase("TestRepo");
+        }
     }
 }
