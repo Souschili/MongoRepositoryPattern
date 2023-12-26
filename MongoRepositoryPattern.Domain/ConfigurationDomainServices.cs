0788649daf8255a8671bc064b1fbb4ee3c9d23ce
Orkhan <aliyevorhan82@gmail.com>
2023-12-27 03:50:36 +0400

    add extention class

---
+++ MongoRepositoryPattern.Domain/ConfigurationDomainServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoRepositoryPattern.Domain.Context;
+
+namespace MongoRepositoryPattern.Domain
+{
+    public static class ConfigurationDomainServices
+    {
+        public static void AddMongoDbContext(this IServiceCollection service)
+        {
+            service.AddSingleton<IMongoDbContext, MongoDbContext>();
+        }
+    }
+}
