9665fc41320cff78d798db29feebbb08a104da43
Orkhan <aliyevorhan82@gmail.com>
2023-12-27 03:51:31 +0400

    add to DI in program mongo context

--- MongoRepositoryPattern.Api/Program.cs
+++ MongoRepositoryPattern.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using MongoRepositoryPattern.Domain;
+
 namespace MongoRepositoryPattern.Api
 {
     public class Program
@@ -8,7 +10,7 @@ namespace MongoRepositoryPattern.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddMongoDbContext();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
