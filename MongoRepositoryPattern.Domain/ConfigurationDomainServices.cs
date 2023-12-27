d970bc465bb56ec8ff03a6faac57f5996d1d3249
Orkhan <aliyevorhan82@gmail.com>
2023-12-27 04:00:07 +0400

    register ImongoDataBase as singletone to not use context/ Alternative way

--- MongoRepositoryPattern.Domain/ConfigurationDomainServices.cs
+++ MongoRepositoryPattern.Domain/ConfigurationDomainServices.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 using MongoRepositoryPattern.Domain.Context;
 
 namespace MongoRepositoryPattern.Domain
 {
     public static class ConfigurationDomainServices
     {
+        
         public static void AddMongoDbContext(this IServiceCollection service)
         {
             service.AddSingleton<IMongoDbContext, MongoDbContext>();
+        }
+
+        static public void AddMongoDataBase(this IServiceCollection service)
+        {
+            service.AddSingleton(cfg =>
+            {
+                var client = new MongoClient();
+                return client.GetDatabase("TestRepo");
+
+            });
         }
     }
 }
