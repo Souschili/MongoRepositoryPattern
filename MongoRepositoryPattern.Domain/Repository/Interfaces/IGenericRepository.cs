7eaf62a138bf6845a09b518139b15fd03e64fa0d
Orkhan <aliyevorhan82@gmail.com>
2023-12-28 03:57:42 +0400

    add genericRepo interface

---
+++ MongoRepositoryPattern.Domain/Repository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace MongoRepositoryPattern.Domain.Repository.Interfaces
+{
+    public interface IGenericRepository<T>
+    {
+        Task CreateAsync(T entity);
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<bool> DeleteAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+
+    }
+}
