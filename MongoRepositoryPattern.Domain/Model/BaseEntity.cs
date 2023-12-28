792489454792ded0e087b247b68899d839749382
Orkhan <aliyevorhan82@gmail.com>
2023-12-28 07:41:43 +0400

    add base class for all entity

---
+++ MongoRepositoryPattern.Domain/Model/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoRepositoryPattern.Domain.Model
+{
+    public class BaseEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public int Id { get; set; }
+    }
+}
