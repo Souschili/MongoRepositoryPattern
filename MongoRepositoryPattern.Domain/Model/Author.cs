01f6974c942173f4715183eb3be5d0d6a69168ba
Orkhan <aliyevorhan82@gmail.com>
2023-12-28 08:33:02 +0400

    add simple midel

---
+++ MongoRepositoryPattern.Domain/Model/Author.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoRepositoryPattern.Domain.Model
+{
+
+    public class Author:BaseEntity
+    {
+        [BsonElement("Name")]
+        public string Name { get; set; }=string.Empty;
+
+        [BsonElement()]
+        public string Description { get; set; } = string.Empty;
+    }
+}
