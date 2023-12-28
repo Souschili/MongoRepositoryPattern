a4ca839fd673b35f2d953dd66001fcd2be31f3aa
Orkhan <aliyevorhan82@gmail.com>
2023-12-28 19:52:52 +0400

    finish attributeclass

--- MongoRepositoryPattern.Domain/Model/Author.cs
+++ MongoRepositoryPattern.Domain/Model/Author.cs
@@ -1,9 +1,10 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoRepositoryPattern.Domain.Custom;
 using MongoRepositoryPattern.Domain.Model.Base;
 
 namespace MongoRepositoryPattern.Domain.Model
 {
-
+    [CollectionName("author")]
     public class Author:BaseEntity
     {
         [BsonElement("Name")]
