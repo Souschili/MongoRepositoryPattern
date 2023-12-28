6b4d80a290200a76f2596a8e3400449529942886
Orkhan <aliyevorhan82@gmail.com>
2023-12-28 19:48:39 +0400

    add custom attribute to keep collection name

---
+++ MongoRepositoryPattern.Domain/Custom/CollectionNameAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace MongoRepositoryPattern.Domain.Custom
+{
+    internal class CollectionNameAttribute:Attribute
+    {
+        public string Name { get; set; }=string.Empty;
+
+        public CollectionNameAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+    }
+}
