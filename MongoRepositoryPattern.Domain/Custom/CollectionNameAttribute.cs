be7d97297ad3ac525b8cdc5b91b322a6d73a6a9c
Orkhan <aliyevorhan82@gmail.com>
2023-12-28 19:49:36 +0400

    change usage for attribure only class

--- MongoRepositoryPattern.Domain/Custom/CollectionNameAttribute.cs
+++ MongoRepositoryPattern.Domain/Custom/CollectionNameAttribute.cs
@@ -1,5 +1,6 @@
 ﻿namespace MongoRepositoryPattern.Domain.Custom
 {
+    [AttributeUsage(AttributeTargets.Class)]
     internal class CollectionNameAttribute:Attribute
     {
         public string Name { get; set; }=string.Empty;
