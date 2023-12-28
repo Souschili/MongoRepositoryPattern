b97544da134108346ad7f897d27c13eb8c1c610e
Orkhan <aliyevorhan82@gmail.com>
2023-12-29 01:04:06 +0400

    make generic anstract class and methods virtual

--- MongoRepositoryPattern.Domain/Repository/GenericRepository.cs
+++ MongoRepositoryPattern.Domain/Repository/GenericRepository.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 namespace MongoRepositoryPattern.Domain.Repository
 {
-    public class GenericRepositoryM<TEntity> : IGenericRepository<TEntity> where TEntity : class, new()
+    public abstract class GenericRepositoryM<TEntity> : IGenericRepository<TEntity> where TEntity : class, new()
     {
         private readonly protected IMongoCollection<TEntity> _collection;
         public GenericRepositoryM(IMongoDatabase database)
@@ -17,27 +17,27 @@ namespace MongoRepositoryPattern.Domain.Repository
             this._collection = database.GetCollection<TEntity>(nameof(TEntity));
         }
 
-        public async Task CreateAsync(TEntity entity)
+        public virtual async Task CreateAsync(TEntity entity)
         {
             await _collection.InsertOneAsync(entity);
         }
 
-        public Task<bool> DeleteAsync(TEntity entity)
+        public virtual Task<bool> DeleteAsync(TEntity entity)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<TEntity>> GetAllAsync()
+        public virtual Task<IEnumerable<TEntity>> GetAllAsync()
         {
             throw new NotImplementedException();
         }
 
-        public Task<TEntity> GetAsync(int id)
+        public virtual Task<TEntity> GetAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> UpdateAsync(TEntity entity)
+        public virtual Task<bool> UpdateAsync(TEntity entity)
         {
             throw new NotImplementedException();
         }
