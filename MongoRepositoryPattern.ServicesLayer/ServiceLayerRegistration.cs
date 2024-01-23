1cad640c6b39827983de6a56ba7f91ea3792cb71
Orkhan <aliyevorhan82@gmail.com>
2024-01-24 03:28:25 +0400

    add extention class

---
+++ MongoRepositoryPattern.ServicesLayer/ServiceLayerRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MongoRepositoryPattern.ServicesLayer
+{
+    static class ServiceLayerRegistration
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
