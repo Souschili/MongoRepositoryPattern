7e8e87a27c296e4282606423a04fd137a5ea47f4
Orkhan <aliyevorhan82@gmail.com>
2024-01-28 02:48:08 +0400

    add repo

--- MongoRepositoryPattern.ServicesLayer/Services/AuthorService.cs
+++ MongoRepositoryPattern.ServicesLayer/Services/AuthorService.cs
@@ -1,10 +1,18 @@
 ﻿using MongoRepositoryPattern.Domain.Model;
+using MongoRepositoryPattern.Domain.Repository.Interfaces;
 using MongoRepositoryPattern.ServicesLayer.Contracts;
 
 namespace MongoRepositoryPattern.ServicesLayer.Services
 {
     public class AuthorService : IAuthorService
     {
+        private readonly IAuthorRepository _authorRepo;
+
+        public AuthorService(IAuthorRepository authorRepo)
+        {
+            _authorRepo = authorRepo;
+        }
+
         public Task AddAuthorAsync(Author author)
         {
             throw new NotImplementedException();
