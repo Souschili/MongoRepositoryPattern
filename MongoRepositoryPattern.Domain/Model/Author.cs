9dec4c8e72f07ca5eeb1f9c292b02554b813e9a4
Orkhan <aliyevorhan82@gmail.com>
2024-01-29 19:11:43 +0400

    fix model

--- MongoRepositoryPattern.Domain/Model/Author.cs
+++ MongoRepositoryPattern.Domain/Model/Author.cs
@@ -8,9 +8,9 @@ namespace MongoRepositoryPattern.Domain.Model
     public class Author:BaseEntity
     {
         [BsonElement("Name")]
-        public string Name { get; set; }=string.Empty;
+        public string FirstName { get; set; }=string.Empty;
 
-        [BsonElement("Description")]
-        public string Description { get; set; } = string.Empty;
+        [BsonElement("LastName")]
+        public string LastName { get; set; } = string.Empty;
     }
 }
